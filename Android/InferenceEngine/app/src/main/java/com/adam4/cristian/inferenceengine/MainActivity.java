9f59ebcf4dbef10ef5900b0ea5b6c9630bc4c79f
Cristian Adam <CristianRAdam@gmail.com>
2015-12-17 14:49:48 -0500

    added initial database code

--- Android/InferenceEngine/app/src/main/java/com/adam4/cristian/inferenceengine/MainActivity.java
+++ Android/InferenceEngine/app/src/main/java/com/adam4/cristian/inferenceengine/MainActivity.java
@@ -1,15 +1,28 @@
 package com.adam4.cristian.inferenceengine;
 
+import android.content.Context;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.SimpleAdapter;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
 
     private Spinner user, session, distraction, param1, param2, param3;
+    ConnectionClass connectionClass;
+
     private static final String[]userOptions = {"John Doe", "Negative Nancy", "Positive Polly"};
     private static final String[]sessionOptions = {"Session 1", "Session 2", "Session 3"};
     private static final String[]distractionOptions = {"Normal Driving", "Spelling Questions", "Relaxing Music"};
@@ -21,6 +34,12 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        connectionClass = new ConnectionClass();
+
+
+        //*************************************************************
+        // Setup GUI menu  ********************************************
+        //*************************************************************
 
         user = (Spinner)findViewById(R.id.User);
         ArrayAdapter<String> userAdapter = new ArrayAdapter<String>(MainActivity.this,
@@ -91,4 +110,42 @@ public class MainActivity extends AppCompatActivity implements AdapterView.OnIte
 
         }
     }
+
+    public class GetResult extends AsyncTask<String, String, String> {
+        String z = "";
+
+        @Override
+        protected void onPostExecute(String r) {
+            Toast.makeText(getApplicationContext(), r, Toast.LENGTH_SHORT).show();
+        }
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                Connection con = connectionClass.CONN();
+                if (con == null) {
+                    z = "Error in connection with SQL server";
+                } else {
+                    String query = "SELECT TOP 1 [ID]\n" +
+                            "      ,[Result]\n" +
+                            "      ,[Result2]\n" +
+                            "  FROM [EnterpriseExpertSystem].[dbo].[MatLabResults]\n" +
+                            "  WHERE Test_Type = ? AND Column1_Test like ? AND Column2_Test like ?;";
+                    PreparedStatement ps = con.prepareStatement(query);
+                    ResultSet rs = ps.executeQuery();
+                    ArrayList data1 = new ArrayList();
+                    while (rs.next()) {
+                        Map<String, String> datanum = new HashMap<String, String>();
+                        datanum.put("A", rs.getString("Id"));
+                        datanum.put("B", rs.getString("ProName"));
+                        datanum.put("C", rs.getString("ProDesc"));
+                    }
+                    z = "Success";
+                }
+            } catch (Exception ex) {
+                z = "Error retrieving data from table";
+            }
+            return z;
+        }
+    }
 }
+
